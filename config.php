aeea17d4989c9ba8c2a3338c703d63ca7a4aeea8
Stefan Bunk <stefan.bunk@googlemail.com>
2014-06-24 13:09:22 +0200

    Added debug print.

--- config.php
+++ config.php
@@ -1,4 +1,12 @@
 <?php
 define('STANBOL_INSTANCE', 'http://localhost:8080/enhancer/');
+define('MINIMUM_CONFIDENCE', 0.0);
+
+function debug_print($v) {
+	echo '<pre>';
+	var_dump($v);
+	echo '</pre>';
+	wp_die();
+}
 //define('STANBOL_INSTANCE', 'http://localhost:8080/enhancer/chain/dbpedia-disambiguation');
 ?>
