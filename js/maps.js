75867396a0388eec2ce4814fb237194fb4e21620
Stefan Bunk <stefan.bunk@googlemail.com>
2014-06-25 01:35:40 +0200

    Added small sleep between the geocode requests.

--- js/maps.js
+++ js/maps.js
@@ -13,39 +13,41 @@
 		var placesNumber = places.length;
 		var placesCount = 0;
 		placesLocations = [];
-		places.forEach(function(place) {
-			maps.geocoder.geocode( { 'address': place.address}, function(results, status) {
-				placesCount += 1;
-				if (status == google.maps.GeocoderStatus.OK) {
-					var geometry = results[0].geometry;
-					var location = 	{
-						place: {
-							lat: geometry.location.lat(),
-							lng: geometry.location.lng()
-						},
-						northEast: {
-							lat: geometry.viewport.getNorthEast().lat(),
-							lng: geometry.viewport.getNorthEast().lng()
-						},
-						southWest: {
-							lat: geometry.viewport.getSouthWest().lat(),
-							lng: geometry.viewport.getSouthWest().lng()
-						},
-						text: place.address,
-						resource: place.resource,
-						selected: place.selected
-					};
-					$("#" + place.id).attr("value", JSON.stringify(location));
-					placesLocations.push(location);
-					console.log("Geocode successful.");
-				} else
-					console.error('Geocode was not successful for the following reason: ' + status);
+		places.forEach(function(place, i) {
+			window.setTimeout(function() {
+				maps.geocoder.geocode({ 'address': place.address}, function (results, status) {
+					placesCount += 1;
+					if (status == google.maps.GeocoderStatus.OK) {
+						var geometry = results[0].geometry;
+						var location = {
+							place: {
+								lat: geometry.location.lat(),
+								lng: geometry.location.lng()
+							},
+							northEast: {
+								lat: geometry.viewport.getNorthEast().lat(),
+								lng: geometry.viewport.getNorthEast().lng()
+							},
+							southWest: {
+								lat: geometry.viewport.getSouthWest().lat(),
+								lng: geometry.viewport.getSouthWest().lng()
+							},
+							text: place.address,
+							resource: place.resource,
+							selected: place.selected
+						};
+						$("#" + place.id).attr("value", JSON.stringify(location));
+						placesLocations.push(location);
+						console.log("Geocode successful.");
+					} else
+						console.error('Geocode was not successful for the following reason: ' + status);
 
-				if (placesCount === placesNumber) {
-					maps.configureMapWithPlaces(placesLocations, map);
-					console.log(placesLocations);
-				}
-			});
+					if (placesCount === placesNumber) {
+						maps.configureMapWithPlaces(placesLocations, map);
+						console.log(placesLocations);
+					}
+				});
+			}, 100 * i);
 		});
 	};
 
