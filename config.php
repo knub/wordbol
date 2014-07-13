9e9400ab3b1086495c140ea54cb599df2be8d652
Stefan Bunk <stefan.bunk@googlemail.com>
2014-07-13 17:08:28 +0200

    Change min confidence.

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('STANBOL_INSTANCE', 'http://localhost:8080/enhancer/chain/wordpress-chain
 // define the http endpoint of the stanbol instance
 define('GOOGLE_MAPS_API_KEY', 'AIzaSyDJAdivC4VOwITwLhtG2Sji4hNFL72fQOY');
 // The minimum confidence for a matched entity to be shown (value in [0..1])
-define('MINIMUM_CONFIDENCE', 0.0);
+define('MINIMUM_CONFIDENCE', 0.40);
 // Define snippet size for the context of the entities
 define('SNIPPET_SIZE', 120);
 // Define whether tags should be added automatically to a post, when entities are selected
